ba5a2626f8b6be47d9dbba74f840e6b747671b2c
JoelFong85 <36076912+JoelFong85@users.noreply.github.com>
2020-06-28 21:58:20 +0800

    First commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+
+   
+}
